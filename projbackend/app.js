43880771fe45982b14ccf02eca82d6dd35aed4a4
npawan98 <npawan98@gmail.com>
2020-12-29 23:00:52 +0530

    started express routing

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
-
-
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require('./routes/auth');
+
+//DB connection
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -12,10 +16,21 @@ mongoose.connect(process.env.DATABASE,{
 }).then(()=>{
     console.log("DB IS CONNECTED");
 }
-)
+);
 
+//middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//my Routes
+app.use("/api", authRoutes);
+
+
+//port
 const port = process.env.PORT || 8000;
 
+//starting a server
 app.listen(port,()=>{
     console.log(`app is running at ${port}`)
 })
